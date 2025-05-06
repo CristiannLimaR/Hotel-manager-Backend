6596f8642c5038d464f663cc8f09c9430122fded
Informatica <informatica@alumno.local>
2025-05-06 15:54:55 -0600

    Facturas en proceso

---
+++ src/invoices/invoice.controller.js
@@ -0,0 +1,8 @@
+import Invoice from "./invoice.model";
+import User from "../users/user.model.js";
+import Hotel from "../hotels/hotel.model.js";
+import Service from "../services/service.model.js";
+
+export const createInvoice = async (req, res) => {
+    
+}
