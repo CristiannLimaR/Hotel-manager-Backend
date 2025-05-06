a3547250017c411a0dbdcf87f10fed91a66651f1
Informatica <informatica@alumno.local>
2025-05-06 16:37:28 -0600

    Factura para Generar

--- src/invoices/invoice.controller.js
+++ src/invoices/invoice.controller.js
@@ -4,5 +4,29 @@ import Hotel from "../hotels/hotel.model.js";
 import Service from "../services/service.model.js";
 
 export const createInvoice = async (req, res) => {
-    
-}
+    const { hotel, user, services, total } = req.body;
+    if (req.user.role !== 'admin') {
+        return res.status(403).json({ message: 'Solo los administradores pueden crear facturas.' });
+    }
+    try {
+        const invoice = new Invoice({
+            user: req.user._id,
+            hotel,
+            services,
+            total,
+        });
+        await invoice.save();
+        
+        res.status(201).json({
+            success: true,
+            message: 'Factura creada exitosamente.',
+            invoice
+        });
+    }catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "No se pudo crear la factura",
+            error
+        });
+    }
+};
